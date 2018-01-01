f108bac33b26385fd35eec54e34481880875aabf
Aathisemithran D <aathi-3034@aathi-3034.csez.zohocorpin.com>
2018-01-01 23:51:12 +0530

    Font related changes done. Pixel sizes fixed.

--- logic.js
+++ logic.js
@@ -377,12 +377,16 @@ sl = {
 			sl.util.switchPage("doencryption", "just_logout_success");
 		},
 		toggleEye : function(){
-			jQuery(".eye_icon_visible").toggle();
-			jQuery(".eye_icon_hidden").toggle();
+			//jQuery(".eye_icon_visible").toggle();
+			//jQuery(".eye_icon_hidden").toggle();
 			if(jQuery(".password_input").attr("type") == "password"){
+				jQuery(".eye_icon_visible").fadeTo(1, "1");
+				jQuery(".eye_icon_hidden").fadeTo(1, "0");
 				jQuery(".password_input").attr("type","text");
 			}
 			else{
+				jQuery(".eye_icon_visible").fadeTo(1, "0");
+				jQuery(".eye_icon_hidden").fadeTo(1, "1");
 				jQuery(".password_input").attr("type","password");
 			}
 		}
